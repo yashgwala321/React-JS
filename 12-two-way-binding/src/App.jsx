f490d6a10f6e420e34b7373d7869773274e14602
yashgwala321 <yashgwala4@gmail.com>
2026-05-29 00:56:41 +0530

    feat: implement basic form handling in React

--- 12-two-way-binding/src/App.jsx
+++ 12-two-way-binding/src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
 function App() {
+
+  const [title, setTitle] = useState('')
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log("Form Submited...." , title)
+    setTitle('')
+  }
+ 
   return (
     <div>
-      
+       <form onSubmit={(e)=>{
+          submitHandler(e)
+       }}>
+          <input 
+          type="text" 
+          placeholder='Enter Your Name'
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value)
+          }}/>
+
+          {/* <h2>{title}</h2> */}
+          <button>Submit</button>
+       </form>
     </div>
   )
 }

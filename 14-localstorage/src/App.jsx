3af66f0e3e9118a5a4165793e70714809625df1b
yashgwala321 <yashgwala4@gmail.com>
2026-05-31 00:19:22 +0530

    feat: practice localStorage with JSON stringify and parse

--- 14-localstorage/src/App.jsx
+++ 14-localstorage/src/App.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
 
 function App() {
+
+  // const user = localStorage.getItem('user')
+  // const age = localStorage.getItem('age')
+
+  // console.log(user,age)
+
+  // localStorage.removeItem('age')
+  // localStorage.removeItem('user')
+
+  const user = {
+    username: "Yash",
+    age: 21,
+    city: 'Ajmer'
+  }
+
+  localStorage.setItem('user', JSON.stringify(user))
+
+  const userShow = JSON.parse(localStorage.getItem('user'))
+  console.log(userShow) 
+
   return (
     <div>
        

ead16af9e3502e2c7a78cbe246d482f1f1399b48
yashgwala321 <yashgwala4@gmail.com>
2026-06-06 22:56:30 +0530

    feat(about): update about page

--- 19-routing-advanced/src/Pages/About.jsx
+++ 19-routing-advanced/src/Pages/About.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Navbar2 from '../Components/Navbar2'
 
 function About() {
-
-  const btnclick = () => {
-    // console.log("Button Clicked !")
-    
-  }
-
   return (
     <div>
-      <button onClick={btnclick} className='bg-emerald-400 font-medium px-5 py-2 rounded m-2 cursor-pointer active:scale-95'>Return To Home</button>
       <h1>About Page</h1>
     </div>
   )
